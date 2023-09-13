558ec6e354f9429702d1ebe2bedb2bec45a93992
Elton Costa <89019551+ryzxxn@users.noreply.github.com>
2023-09-13 18:17:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default function App() {
   // Use the fetch API to make a GET request to the URL
   async function getdata() {
     try {
-      const response = await fetch('https://api.waifu.pics/sfw/waifu');
+      const response = await fetch('https://api.waifu.pics/nsfw/waifu');
 
       if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
