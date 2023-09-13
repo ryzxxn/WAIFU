721cddce9dbd78e9a5545d5c647115eed6bd888d
Elton Costa <89019551+ryzxxn@users.noreply.github.com>
2023-09-13 20:13:21 +0530

    Update logic.js

--- src/module/logic.js
+++ src/module/logic.js
@@ -2,14 +2,16 @@ import React from 'react';
 
 export default function Logic({ imgSrc }) {
   return (
-    <div className='viewport'>
-        <h1>Anime Waifu</h1>
-        {console.log(imgSrc)}
-      {imgSrc ? (
-        <img className='img' src={imgSrc} alt="Random Waifu" />
-      ) : (
-        <p>Loading image...</p>
-      )}
-    </div>
+    <>
+      <div className='viewport'>
+          <h1>Anime Waifu</h1>
+          {console.log(imgSrc)}
+        {imgSrc ? (
+          <img className='img' src={imgSrc} alt="Random Waifu" />
+        ) : (
+          <p>Loading image...</p>
+        )}
+      </div>
+    </>
   );
 }
