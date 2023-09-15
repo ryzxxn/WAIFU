3c53d3350ca07f4446d16a17e86fdb906b52ac45
Elton Costa <89019551+ryzxxn@users.noreply.github.com>
2023-09-15 15:53:52 +0530

    added nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './style.css';
 import Logic from './module/logic';
+import Navbar from './module/navbar';
 
 
 export default function App() {
   return (
     <>
+    <Navbar/>
     <div className='daddy'>
       <Logic/>
     </div>
---
+++ src/module/navbar.js
@@ -0,0 +1,9 @@
+export default function Navbar() {
+  return (
+    <>
+    <div className='navbar'>
+        <a className='links' href='https://github.com/ryzxxn/WAIFU.exe'>Github</a>
+    </div>
+    </>
+  )
+}
